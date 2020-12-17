387c1c553f30f71ee3146b5d63307eb22e0abffd
grace-mugoiri <gracemugoiri@gmail.com>
2020-12-17 17:01:10 +0300

    clean up code

--- project/auth.py
+++ project/auth.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+	return render_template('login.html')
 
 @auth.route('/login', methods=["POST"])
 def login_post():
@@ -26,6 +30,9 @@ def login_post():
 	login_user(user, remember=remember)
 	return redirect(url_for('main.profile'))
 
+@auth.route('/signup')
+def signup():
+	return render_template('signup.html')
 
 @auth.route('/signup', methods=["POST"])
 def signup_post():
@@ -50,6 +57,9 @@ def signup_post():
 
 	return redirect(url_for('auth.login'))
 
+
 @auth.route('/logout')
+@login_required
 def logout():
-	return 'Logout'
+	logout_user()
+	return redirect(url_for('main.index'))
